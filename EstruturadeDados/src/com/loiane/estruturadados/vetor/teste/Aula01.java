aa94809ef6d754fdadc69dad3a509c6a52e77e17
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-04-17 07:01:04 -0300

    first commit

---
+++ EstruturadeDados/src/com/loiane/estruturadados/vetor/teste/Aula01.java
@@ -0,0 +1,11 @@
+package com.loiane.estruturadados.vetor.teste;
+
+import com.loiane.estruturadados.vetor.Vetor;
+
+public class Aula01 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+        int i = 0;
+        i++;
+    }
+}
