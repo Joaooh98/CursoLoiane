23402c92382427b6d57450cfd102012ce3b6370d
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-05-28 22:34:22 -0300

    first commit

--- EstruturadeDados/src/com/loiane/estruturadados/vetor/teste/Aula02.java
+++ EstruturadeDados/src/com/loiane/estruturadados/vetor/teste/Aula02.java
@@ -7,6 +7,13 @@ public class Aula02 {
         Vetor vetor = new Vetor(5);
         int i = 0;
         i++;
+
+        int[] vetorInteiro = {1, 2, 3, 4, 5};
+        double[] vetorDouble = {1.5, 2.7, 3.2, 4.9, 5.1};
+        String[] vetorString = {"cachorro", "gato", "rato", "elefante"};
+        boolean[] vetorBooleano = {true, false, true, false};
+        int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        // MinhaClasse[] vetorObjeto = {objeto1, objeto2, objeto3};
     }
 
 }
