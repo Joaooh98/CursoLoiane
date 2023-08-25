195c24c97e488adfbf2c3745fb378d3600444b8d
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-08-25 18:51:52 -0300

    teste trabalho

--- EstruturadeDados/src/com/loiane/javaBasico/Thread/Repository/OrderRepository.java
+++ EstruturadeDados/src/com/loiane/javaBasico/Thread/Repository/OrderRepository.java
@@ -8,15 +8,7 @@ public class OrderRepository {
     
     public List<Order> listOrdes = List.of(
         new Order(1),
-        new Order(2),
-        new Order(3),
-        new Order(4),
-        new Order(5),
-        new Order(6),
-        new Order(7),
-        new Order(8),
-        new Order(9),
-        new Order(10));
+        new Order(2));
 
     public List<Order> listOrdes() {
         return listOrdes;
--- EstruturadeDados/src/com/loiane/javaBasico/Thread/test/OrderControllerwithThreads.java
+++ EstruturadeDados/src/com/loiane/javaBasico/Thread/test/OrderControllerwithThreads.java
@@ -9,13 +9,13 @@ public class OrderControllerwithThreads {
     public static void main(String[] args) {
         for (int i = 0; i < 30; i++) {
 
-            Thread thread2 = new Thread(() -> loup());
+            Thread thread2 = new Thread(() -> loupis());
             thread2.start();
-            Thread thread3 = new Thread(() -> loup());
+            Thread thread3 = new Thread(() -> loupi());
             thread3.start();
-            Thread thread4 = new Thread(() -> loup());
+            Thread thread4 = new Thread(() -> loupi());
             thread4.start();
-            Thread thread5 = new Thread(() -> loup());
+            Thread thread5 = new Thread(() -> loupis());
             thread5.start();
             Thread thread6 = new Thread(() -> loup());
             thread6.start();
@@ -23,16 +23,54 @@ public class OrderControllerwithThreads {
             thread7.start();
             Thread thread8 = new Thread(() -> loup());
             thread8.start();
-            Thread thread9 = new Thread(() -> loup());
+            Thread thread9 = new Thread(() -> loupis());
             thread9.start();
-            Thread thread10 = new Thread(() -> loup());
+            Thread thread10 = new Thread(() -> loupi());
             thread10.start();
             Thread thread11 = new Thread(() -> loup());
             thread11.start();
         }
     }
 
-    public static void loup() {
+    public static synchronized void loup() {
+        
+            var orderRepository = new OrderRepository();
+
+            List<Order> list = orderRepository.listOrdes();
+
+            for (Order order : list) {
+                synchronized(order){
+                    System.out.println("Thread synchronized(order.getId() " + Thread.currentThread().getName()
+                    + " está dentro do bloco synchronized para Order: " + order.getId());
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            
+        }
+    }
+
+    public static synchronized void loupi() {
+        
+            var orderRepository = new OrderRepository();
+
+            List<Order> list = orderRepository.listOrdes();
+
+            for (Order order : list) {
+                System.out.println("Thread synchronized metodo" + Thread.currentThread().getName()
+                        + " está dentro do bloco synchronized para Order: " + order.getId());
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            
+        }
+    }
+
+    public static void loupis() {
         var Order = new Order(1);
         synchronized (Order.getId()) {
             var orderRepository = new OrderRepository();
@@ -40,7 +78,7 @@ public class OrderControllerwithThreads {
             List<Order> list = orderRepository.listOrdes();
 
             for (Order order : list) {
-                System.out.println("Thread " + Thread.currentThread().getName()
+                System.out.println("Thread var Order = new Order(1);" + Thread.currentThread().getName()
                         + " está dentro do bloco synchronized para Order: " + order.getId());
             }
             try {
