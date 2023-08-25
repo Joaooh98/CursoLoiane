8f317e994d7e0b9509384ef4e279f4b3399f50cd
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-08-25 08:51:06 -0300

    25/08/2023

--- EstruturadeDados/src/com/loiane/javaBasico/Thread/domain/Order.java
+++ EstruturadeDados/src/com/loiane/javaBasico/Thread/domain/Order.java
@@ -2,7 +2,6 @@ package com.loiane.javaBasico.Thread.domain;
 
 public class Order {
     private Integer id;
-
     
     public Order(Integer id) {
         this.id = id;
--- EstruturadeDados/src/com/loiane/javaBasico/Thread/test/OrderControllerwithThreads.java
+++ EstruturadeDados/src/com/loiane/javaBasico/Thread/test/OrderControllerwithThreads.java
@@ -7,43 +7,47 @@ import com.loiane.javaBasico.Thread.domain.Order;
 
 public class OrderControllerwithThreads {
     public static void main(String[] args) {
+        for (int i = 0; i < 30; i++) {
+
+            Thread thread2 = new Thread(() -> loup());
+            thread2.start();
+            Thread thread3 = new Thread(() -> loup());
+            thread3.start();
+            Thread thread4 = new Thread(() -> loup());
+            thread4.start();
+            Thread thread5 = new Thread(() -> loup());
+            thread5.start();
+            Thread thread6 = new Thread(() -> loup());
+            thread6.start();
+            Thread thread7 = new Thread(() -> loup());
+            thread7.start();
+            Thread thread8 = new Thread(() -> loup());
+            thread8.start();
+            Thread thread9 = new Thread(() -> loup());
+            thread9.start();
+            Thread thread10 = new Thread(() -> loup());
+            thread10.start();
+            Thread thread11 = new Thread(() -> loup());
+            thread11.start();
+        }
+    }
+
+    public static void loup() {
         var Order = new Order(1);
+        synchronized (Order.getId()) {
+            var orderRepository = new OrderRepository();
 
-        Thread thread1 = new Thread(() -> {
-            synchronized (Order) {
-                var orderRepository = new OrderRepository();
+            List<Order> list = orderRepository.listOrdes();
 
-                List<Order> list = orderRepository.listOrdes();
-
-                for (Order order : list) {
-                    System.out.println("Thread 1 está dentro do bloco synchronized para Order: " + order.getId());
-                }
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            for (Order order : list) {
+                System.out.println("Thread " + Thread.currentThread().getName()
+                        + " está dentro do bloco synchronized para Order: " + order.getId());
             }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            synchronized (Order) {
-                var orderRepository = new OrderRepository();
-
-                List<Order> list = orderRepository.listOrdes();
-
-                for (Order order : list) {
-                    System.out.println("Thread 2 está dentro do bloco synchronized para Order: "+ order.getId());
-                }
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
-
-        thread1.start();
-        thread2.start();
+        }
     }
 }
